eeea67f731c068e5c3ab424ec431aa51093d87ce
YAOU Reda <yaoureda24@gmail.com>
2026-02-06 15:40:11 +0100

    deleting useless user=current

--- app/web/tasks_routes.py
+++ app/web/tasks_routes.py
@@ -1,7 +1,7 @@
-from flask_login import current_user
+
 from .config import web_bp
 from flask import render_template
 
 @web_bp.route('/tasks')
 def tasks():
-    return render_template('tasks.html', user=current_user)
+    return render_template('tasks.html')
